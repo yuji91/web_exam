46b9e5c1b031f1899f52024801120c066811f1de
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-08-11 21:57:11 +0900

    init

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,40 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
+
+listen '/Users/saitouhitoha/blog_app/tmp/unicorn.sock'
+pid    '/Users/saitouhitoha/blog_app/tmp/unicorn.pid'
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
+
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+#web: bin/start-nginx bundle exec unicorn -c config/unicorn.rb
+
+require 'fileutils'
+preload_app true
+timeout 5
 worker_processes 4
-listen 'unix:///tmp/nginx.socket', backlog: 1024
+listen '/tmp/nginx.socket', backlog: 1024
 
 before_fork do |server,worker|
-    FileUtils.touch('/tmp/app-initialized')
+	FileUtils.touch('/tmp/app-initialized')
 end
