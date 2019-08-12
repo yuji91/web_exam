892c8bb73a1299d995edc9e96e82f241c3bb1e37
yuji91 <yuji91.1227@gmail.com>
2019-08-12 21:38:16 +0900

    Fix unicone setting

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,9 +2,6 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
-listen '/Users/saitouhitoha/blog_app/tmp/unicorn.sock'
-pid    '/Users/saitouhitoha/blog_app/tmp/unicorn.pid'
-
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
@@ -22,17 +19,4 @@ after_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
-end
-
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-
-require 'fileutils'
-preload_app true
-timeout 5
-worker_processes 4
-listen '/tmp/nginx.socket', backlog: 1024
-
-before_fork do |server,worker|
-	FileUtils.touch('/tmp/app-initialized')
 end
